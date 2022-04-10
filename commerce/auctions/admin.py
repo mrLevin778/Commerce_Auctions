f20124bf25cacaafdeee9ffbec2d4300b079a225
mrLevin778 <92675096+mrLevin778@users.noreply.github.com>
2022-04-10 11:36:38 +0300

    first commit

---
+++ commerce/auctions/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Category, Auction
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Category, CategoryAdmin)
+
+class AuctionAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'start_price', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['start_price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Auction, AuctionAdmin)
---
+++ commerce/auctions/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Auction(models.Model):
+    category = models.ForeignKey(Category, related_name='auctions', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='auctions/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    start_price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
